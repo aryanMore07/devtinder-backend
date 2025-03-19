e6c31fc984ce14a46b41fcd589fea475050830e5
aryanMore07 <aryanmore498@gmail.com>
2025-03-19 14:44:27 +0530

    Playing with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Aryan", lastName: "More" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // res.send("Route handler One");
+    console.log("Handling the route user!!");
+    // res.send("Response!!");
+    next();
+  },
+  (req, res) => {
+    // res.send("Route handler One");
+    console.log("Handling the route user 2!!");
+    res.send("2nd Response!!");
+  }
+);
 
 app.listen(7777, (req, res) => {
   console.log(`Express server is running on http://localhost:7777/`);
