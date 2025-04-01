2e1cb789a2606259b0feebc68917c2504e4d5cde
aryanMore07 <aryanmore498@gmail.com>
2025-04-01 11:28:19 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -12,7 +12,80 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.send("User Added Succesfully!");
   } catch (error) {
+    console.log(error);
     res.status(400).send("Error saving the user data!" + error.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const data = await User.find();
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(400).send("Error fetching the users data!" + error.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  const email = req.body.emailId;
+  try {
+    const user = await User.findOne();
+    if (!user) {
+      res.status(404).send(`No user found by email: ${email}`);
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      res.status(404).send(`No user found by email: ${email}`);
+    }
+    res.status(200).send("User deleted successfully!");
+  } catch (error) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  const ALLOWED_UPDATES = [
+    "userId",
+    "photoUrl",
+    "about",
+    "gender",
+    "age",
+    "skills",
+    "password",
+  ];
+
+  try {
+    const isUpdatedAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdatedAllowed) {
+      throw new Error("Update not allowed!");
+    }
+
+    if (data.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
+    await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("UPDATE FAILED: " + error.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,52 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg",
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: { type: String },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = model("User", userSchema);
 
